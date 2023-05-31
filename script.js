0edf74a596de6c9c95083e1e3edba3ff1910d81e
Mavericks Balitaan <balitaanmavericks@gmail.com>
2023-05-31 07:45:16 +0500

    Add cards section

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ let searchBtn = document.querySelector("#searchBtn");
 let container = document.querySelector(".container");
 let img = document.querySelector(".img");
 let info = document.querySelector(".info");
+let cards = document.querySelector(".cards");
 
+// Default Movies
+const movies = ["Iron Man", "Avengers", "Guardians of the Galaxy", "Thor", "Hulk", "Doctor Strange"];
 
 // Base API URL
 let baseApiUrl = "https://www.omdbapi.com/?apikey=52cc332f&t=";
 
 // Search Functionality
 function searchFunc() {
+  // Clears the previous content
+  img.innerHTML = "";
+  info.innerHTML = "";
+
   let apiUrl = baseApiUrl + searchInput.value;
 
   fetch(apiUrl).then(res => res.json()).then(data => {
@@ -50,6 +57,9 @@ function searchFunc() {
     info.append(title, year, rated, released, runtime, genre, director, writer, actors, plot);
     container.append(img, info);
   });
+
+  // Clears the search input
+  searchInput.value = "";
 }
 
 // When Enter Key is Pressed
@@ -66,4 +76,44 @@ searchBtn.addEventListener("click", searchFunc);
 window.addEventListener("keypress", (e) => {
   enterKeyFunc(e);
 });
+
+function defaultSearch(arr) {
+  arr = movies;
+
+  arr.map(movie => {
+    console.log(movie);
+    fetch(`${baseApiUrl} + ${movie}`).then(res => res.json()).then(data => {
+      console.log(data);
+
+      let cardDiv = document.createElement("div");
+      cardDiv.classList.add("card");
+
+      let imgEl = document.createElement("img");
+      imgEl.classList.add("card-img-top");
+      imgEl.src = data.Poster;
+      
+      let cardBodyDiv = document.createElement("div");
+      cardBodyDiv.classList.add("card-body");
+
+      let cardTitle = document.createElement("h5");
+      cardTitle.classList.add("card-title");
+      cardTitle.textContent = data.Title;
+
+      let cardText = document.createElement("p");
+      cardText.classList.add("card-text");
+      cardText.textContent = data.Year;
+
+      cardBodyDiv.append(cardTitle, cardText);
+      cardDiv.append(imgEl, cardBodyDiv);
+      cards.appendChild(cardDiv);
+    })
+  })
+}
+
+// DOMContent is loaded
+window.addEventListener("DOMContentLoaded", defaultSearch);
+window.addEventListener("load", () => {
+  searchInput.focus();
+})
+
 
